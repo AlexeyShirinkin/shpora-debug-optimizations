b075270682400ea02967714e722ce6887319c7e7
Alexey <winderton228@yandex.ru>
2023-06-18 13:13:00 +0500

    Add test om compression and decompression

---
+++ optimizations/JPEG/Compressor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using JPEG.Images;
+
+namespace JPEG
+{
+    public class Compressor
+    {
+        public CompressedImage Compress(Matrix matrix, int quality = 50)
+        {
+            var allQuantizedBytes = new List<byte>();
+
+            for(var y = 0; y < matrix.Height; y += MatrixOperations.DCTSize)
+            {
+                for(var x = 0; x < matrix.Width; x += MatrixOperations.DCTSize)
+                {
+                    foreach (var selector in new Func<Pixel, double>[] {p => p.Y, p => p.Cb, p => p.Cr})
+                    {
+                        var subMatrix = MatrixOperations.GetSubMatrix(matrix, y, MatrixOperations.DCTSize, x, MatrixOperations.DCTSize, selector);
+                        MatrixOperations.ShiftMatrixValues(subMatrix, -128);
+                        var channelFreqs = DCT.DCT2D(subMatrix);
+                        var quantizedFreqs = MatrixOperations.Quantize(channelFreqs, quality);
+                        var quantizedBytes = MatrixOperations.ZigZagScan(quantizedFreqs);
+                        allQuantizedBytes.AddRange(quantizedBytes);
+                    }
+                }
+            }
+
+            long bitsCount;
+            Dictionary<BitsWithLength, byte> decodeTable;
+            var compressedBytes = HuffmanCodec.Encode(allQuantizedBytes, out decodeTable, out bitsCount);
+
+            return new CompressedImage {Quality = quality, CompressedBytes = compressedBytes, BitsCount = bitsCount, DecodeTable = decodeTable, Height = matrix.Height, Width = matrix.Width};
+        }
+    }
+}
